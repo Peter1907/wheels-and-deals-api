75a58d0e94476b654412b026c43501491e417b47
CindyDorantes <cindym.dorantes@gmail.com>
2023-02-27 15:36:23 -0600

    Create test files for models and requests

---
+++ spec/models/reservation_spec.rb
@@ -0,0 +1,23 @@
+# require 'rails_helper'
+
+# RSpec.describe User, type: :model do
+#   # tests go here
+#   before :each do
+#     @user = User.create(
+#       name: 'Microverse',
+#       email: 'd@gmail.com',
+#       password: 'password'
+#     )
+#   end
+
+#   context 'Test user validations' do
+#     it 'is valid with valid attributes' do
+#       expect(@user).to be_valid
+#     end
+
+#     it 'name must not be blank' do
+#       @user.name = nil
+#       expect(@user).to_not be_valid
+#     end
+#   end
+# end

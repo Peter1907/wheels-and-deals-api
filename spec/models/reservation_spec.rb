beb8b7d5c43eeb2806482b371e96771e4afca6c8
CindyDorantes <cindym.dorantes@gmail.com>
2023-02-27 16:18:50 -0600

    Add rspec test for models

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,23 +1,46 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   # tests go here
-#   before :each do
-#     @user = User.create(
-#       name: 'Microverse',
-#       email: 'd@gmail.com',
-#       password: 'password'
-#     )
-#   end
+RSpec.describe Reservation, type: :model do
+  # tests go here
+  before :each do
+    @car_first = Car.create(
+      name: 'Bettle',
+      photo: 'photo_link',
+      description: 'Nice yellow car',
+      price: 25_000,
+      reservation_fee: 250
+    )
+    @user_first = User.create(
+      name: 'Microverse',
+      email: 'test@gmail.com'
+    )
+    @reservation = Reservation.create(
+      date: '2024-10-10',
+      city: 'Mexico City',
+      country: 'Mexico',
+      user_id: @user_first.id,
+      car_id: @car_first.id
+    )
+  end
 
-#   context 'Test user validations' do
-#     it 'is valid with valid attributes' do
-#       expect(@user).to be_valid
-#     end
+  context 'Test reservation validations' do
+    it 'is valid with valid attributes' do
+      expect(@reservation).to be_valid
+    end
 
-#     it 'name must not be blank' do
-#       @user.name = nil
-#       expect(@user).to_not be_valid
-#     end
-#   end
-# end
+    it 'date must not be blank' do
+      @reservation.date = nil
+      expect(@reservation).to_not be_valid
+    end
+
+    it 'city must not be blank' do
+      @reservation.city = nil
+      expect(@reservation).to_not be_valid
+    end
+
+    it 'country must not be blank' do
+      @reservation.country = nil
+      expect(@reservation).to_not be_valid
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'support/feature_helpers'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
@@ -33,8 +32,6 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{Rails.root}/spec/fixtures"
-
-  config.include FeatureHelpers, type: :feature
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -85,4 +82,3 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 end
-

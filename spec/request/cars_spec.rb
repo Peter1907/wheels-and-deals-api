128ec3e553e409dd4e5282badb63a93f89766454
Peter1907 <peter.nady.1408@gmail.com>
2023-02-28 18:43:02 +0200

    Add tests for cars requests

--- spec/request/cars_spec.rb
+++ spec/request/cars_spec.rb
@@ -1,5 +1,57 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Cars', type: :request do
+RSpec.describe 'Cars', type: :request do
+  before(:each) do
+    @car_first = Car.create(name: 'Bettle', photo: 'photo_link', description: 'Nice yellow car', price: 25000)
+    @car_second = Car.create(name: 'Camaro', photo: 'photo_link', description: 'Nice red car', price: 30000)
+  end
 
-# end
+  describe 'Cars index path' do
+    before { get cars_path }
+
+    it 'returns all cars' do
+      expect(response.body).to include(@car_first.name)
+      expect(response.body).to include(@car_second.name)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+  
+  describe 'Cars show path' do
+    before { get car_path(@car_first.id) }
+
+    it 'returns a specific car' do
+      expect(response.body).to include(@car_first.name, @car_first.description)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'Cars create path' do
+    before(:each) do
+      @params = {
+        name: 'Audi',
+        photo: 'photo_link',
+        description: 'Nice black car',
+        price: 300000
+      }
+    end
+
+    it 'returns status code 201 when car is created' do
+      post cars_path, params: @params
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'returns the errors when the params ara missing' do
+      @params[:name] = nil
+      @params[:price] = "abc"
+      post cars_path, params: @params
+      expect(response.body).to include('"name":["can\'t be blank"]')
+      expect(response.body).to include('"price":["is not a number"]')
+    end
+  end
+end

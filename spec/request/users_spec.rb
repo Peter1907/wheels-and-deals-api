b001805802149d6773043f017f1abcd9f08aa1a5
IngDavidTM <david-1011@hotmail.com>
2023-02-28 12:26:17 -0500

    Add users spec

--- spec/request/users_spec.rb
+++ spec/request/users_spec.rb
@@ -1,5 +1,65 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Users', type: :request do
+RSpec.describe 'Users', type: :request do
+  let(:user) do
+    User.create(name: 'Test', email: 'test@example.com', password: 'password')
+  end
 
-# end
+  describe 'GET /users' do
+    before do
+      post '/login', params: { email: user.email, password: user.password }
+      token = JSON.parse(response.body)['token']
+      get '/users', headers: { 'Authorization' => "Bearer #{token}" }
+    end
+
+    it 'returns all users' do
+      expect(response.body).to include(user.name)
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns users' do
+      expect(response.body).to include('Test')
+    end
+
+    it 'returns http unauthorized' do
+      post '/login', params: { email: user.email, password: 'invalid' }
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+
+  describe 'GET /users/:id' do
+    before do
+      post '/login', params: { email: user.email, password: user.password }
+      token = JSON.parse(response.body)['token']
+      get user_path(user.id), headers: { 'Authorization' => "Bearer #{token}" }
+    end
+
+    it 'returns user' do
+      expect(response.body).to include(user.name)
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns http unauthorized' do
+      post '/login', params: { email: user.email, password: 'invalid' }
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+
+  describe 'POST /users' do
+    it 'creates a user' do
+      post '/users', params: { name: 'Test', email: 'test@example.com', password: 'password' }
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'returns error when the params are missing' do
+      post '/users', params: { name: 'Test', email: '', password: 'password' }
+      expect(response.body).to include('"email":["can\'t be blank"]')
+    end
+  end
+end

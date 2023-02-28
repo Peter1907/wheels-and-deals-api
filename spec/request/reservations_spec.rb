676df842023451f59800989084e2bfc8a6e66332
Diego Mero <diegomero_1999@hotmail.com>
2023-02-28 10:46:05 -0500

    Add integration test for reservations

--- spec/request/reservations_spec.rb
+++ spec/request/reservations_spec.rb
@@ -1,5 +1,10 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Users', type: :request do
-
-# end
+RSpec.describe 'Reservations', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get 'api/reservations'
+      expect(response).to have_http_status(:success)
+    end
+  end
+end

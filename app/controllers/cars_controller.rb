44974ee03f218c8b0970487fa01eea335ffa596a
Peter1907 <peter.nady.1408@gmail.com>
2023-02-28 20:27:08 +0200

    Fix linter errors

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,5 @@
 class CarsController < ApplicationController
-  skip_before_action :authenticate_request, only: [:index, :show]
+  skip_before_action :authenticate_request, only: %i[index show]
 
   def index
     @cars = Car.all

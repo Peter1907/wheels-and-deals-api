34f3002253fda7f7f28ee38e3c2b1b35ca42bb5f
IngDavidTM <david-1011@hotmail.com>
2023-02-28 10:41:40 -0500

    Implement the required changes

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -7,7 +7,7 @@ class AuthenticationController < ApplicationController
       token = jwt_encode(user_id: @user.id)
       render json: { token: }, status: :ok
     else
-      render json: { error: 'Invalid username or password' }, status: :unauthorized
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
   end
 end
